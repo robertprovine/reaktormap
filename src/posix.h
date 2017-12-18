c6159649585b18adc9a94a7d60cfbac359c9b952
robertprovine <robertprovine@gmail.com>
2017-12-18 16:08:03 -0500

    cleared unnecessary headers

--- src/posix.h
+++ src/posix.h
@@ -1,14 +1,7 @@
 #ifndef POSIX__H
 #define POSIX__H
 
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <dirent.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <limits.h>
 
 int IsDirectory(const char* path);
 
